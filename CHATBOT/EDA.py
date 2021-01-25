d3d173e45b4f5396a0a8c09d3f1aa303197e8aa9
Park <qkrtmdwls280@gmail.com>
2021-01-25 19:39:07 +0900

    add

---
+++ CHATBOT/EDA.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import Counter
+from konlpy.tag import Okt
+from functools import reduce
+from wordcloud import WordCloud
+
+DATA_IN_PATH = './data_in/'
+data = pd.read_csv(DATA_IN_PATH + 'ChatBotData.csv', encoding='utf-8')
+sentences = list(data['Q']) + list(data['A'])
+tokenized_sentences = [s.split() for s in sentences]
+sent_len_by_token = [len(t) for t in tokenized_sentences]
+sent_len_by_eumjeol = [len(s.replace(' ', '')) for s in sentences]
+
+okt = Okt()
+
+morph_tokenized_sentences = [okt.morphs(s.replace(' ', '')) for s in sentences]
+sent_len_by_morph = [len(t) for t in morph_tokenized_sentences]
+plt.figure(figsize=(12, 5))
+plt.hist(sent_len_by_token, bins=50, range=[0,50], alpha=0.5, color= 'r', label='eojeol')
+plt.hist(sent_len_by_morph, bins=50, range=[0,50], alpha=0.5, color='g', label='morph')
+plt.hist(sent_len_by_eumjeol, bins=50, range=[0,50], alpha=0.5, color='b', label='eumjeol')
+plt.title('Sentence Length Histogram')
+plt.xlabel('Sentence Length')
+plt.ylabel('Number of Sentences')
+plt.show()
